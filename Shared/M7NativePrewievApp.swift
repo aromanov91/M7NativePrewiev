e18054868f58a05dc79d6048938cddc44e63a29d
Alexander Romanov <aromanov07@gmail.ru>
2020-09-11 22:23:28 +0300

    Initial Commit

---
+++ Shared/M7NativePrewievApp.swift
@@ -0,0 +1,17 @@
+//
+//  M7NativePrewievApp.swift
+//  Shared
+//
+//  Created by 18391981 on 11.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct M7NativePrewievApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
